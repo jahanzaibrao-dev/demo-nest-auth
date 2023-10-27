581f67063f7619866959959a311c908fb762442f
jahanzaibrao-dev <jahanzaib@tetrahex.com>
2023-10-27 18:55:55 +0500

    test: login and email endpoints

--- src/auth/dto/verifyEmail.dto.ts
+++ src/auth/dto/verifyEmail.dto.ts
@@ -1,16 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsDefined,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class SendEmailDto {
   @ApiProperty({ type: String, name: 'email', example: 'johnsnow@gmail.com' })
   @IsEmail({}, { message: 'Invalid Email!' })
   @IsNotEmpty({ message: 'Email must not be empty' })
   @IsString({ message: 'Email must be a string' })
+  @IsDefined({ message: 'Email must be defined' })
   readonly email: string;
 }
 
 export class VerifyEmailDto extends SendEmailDto {
   @ApiProperty({ type: Number, name: 'code', example: 123456 })
   @IsNumber({}, { message: 'code must be a number' })
+  @IsDefined({ message: 'code must be defined' })
+  @Min(100000, { message: 'Code must have exactly 6 digits' })
+  @Max(999999, { message: 'Code must have exactly 6 digits' })
   readonly code: number;
 }
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -6,11 +6,14 @@ import mongoose, { Model } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
 import {
   createUserDTOMock,
+  loginDtoMock,
   mockedRegisterResponse,
+  unverifiedUserWithOtpMock,
 } from 'src/auth/mocks/auth.mocks';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from 'src/user/schemas/user.schema';
-import { MailerModule, MailerService } from '@nestjs-modules/mailer';
+import { MailerService } from '@nestjs-modules/mailer';
+import { VerifyEmailDto } from 'src/auth/dto/verifyEmail.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,24 +23,7 @@ describe('AppController (e2e)', () => {
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        AppModule,
-        MailerModule.forRoot({
-          transport: {
-            host: 'testHost',
-            port: 100,
-            secure: false,
-            auth: {
-              user: 'testUser@example.com',
-              pass: '123456jdh',
-            },
-          },
-          defaults: {
-            from: 'testUser@example.com',
-          },
-          preview: true,
-        }),
-      ],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -227,14 +213,17 @@ describe('AppController (e2e)', () => {
   });
 
   describe('sendEmail', () => {
+    let savedUser;
+    let spySendMail: jest.SpyInstance;
+
     beforeEach(async () => {
       const hashedPass = await bcrypt.hash(createUserDTOMock.password, 5);
       const mockUser = {
         ...createUserDTOMock,
         password: hashedPass,
       };
-      await userModel.create(mockUser);
-      jest.spyOn(mailerService, 'sendMail');
+      savedUser = await userModel.create(mockUser);
+      spySendMail = jest.spyOn(mailerService, 'sendMail');
     });
 
     it('should_send_an_otp_to_user_email_successfully', async () => {
@@ -248,6 +237,429 @@ describe('AppController (e2e)', () => {
         .expect(201);
 
       expect(response.body).toEqual(expectedResponse);
+    });
+
+    it('should_throw_an_exception_if_user_does_not_exist', async () => {
+      const mockPayload = {
+        email: 'wrongEmail@gmail.com',
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toEqual(
+        `User with this email doesn't exist`,
+      );
+    });
+
+    it('should_throw_an_exception_if_user_is_already_verified', async () => {
+      savedUser.isVerified = true;
+      await savedUser.save();
+      const mockPayload = {
+        email: savedUser.email,
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(403);
+
+      expect(response.body.message).toEqual(`User already verified`);
+    });
+
+    it('should_throw_an_exception_if_mailer_failed_to_send_email', async () => {
+      spySendMail.mockRejectedValueOnce('Failed to send email');
+      const response = await request(server)
+        .post('/auth/email')
+        .send({ email: createUserDTOMock.email })
+        .expect(500);
+
+      expect(response.body.message).toEqual(`Something went wrong!`);
+    });
+
+    it('should_throw_an_exception_if_email_is_not_defined', async () => {
+      const mockPayload = {
+        email: undefined,
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be defined');
+    });
+
+    it('should_throw_an_exception_if_email_is_empty', async () => {
+      const mockPayload = {
+        email: '',
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must not be empty');
+    });
+
+    it('should_throw_an_exception_if_email_format_is_invalid', async () => {
+      const mockPayload = {
+        email: 'invalidEmail',
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Invalid Email!');
+    });
+
+    it('should_throw_an_exception_if_email_is_not_a_string', async () => {
+      const mockPayload = {
+        email: 1234,
+      };
+      const response = await request(server)
+        .post('/auth/email')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be a string');
+    });
+  });
+
+  describe('verifyEmail', () => {
+    let savedUser;
+
+    beforeEach(async () => {
+      const hashedPass = await bcrypt.hash(createUserDTOMock.password, 5);
+      const mockUser = {
+        ...createUserDTOMock,
+        password: hashedPass,
+        otp: unverifiedUserWithOtpMock.otp,
+      };
+      savedUser = await userModel.create(mockUser);
+    });
+
+    it('should verify a user successfully using otp', async () => {
+      const payload: VerifyEmailDto = {
+        email: createUserDTOMock.email,
+        code: savedUser.otp,
+      };
+
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(payload)
+        .expect(201);
+
+      const updatedUser = await userModel.findOne({ email: payload.email });
+
+      expect(response.body.message).toEqual('User verified successfully');
+      expect(updatedUser.isVerified).toBe(true);
+    });
+
+    it('should_throw_an_exception_if_user_does_not_exist', async () => {
+      const mockPayload = {
+        email: 'wrongEmail@gmail.com',
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toEqual(
+        `User with this email doesn't exist`,
+      );
+    });
+
+    it('should_throw_an_exception_if_user_is_already_verified', async () => {
+      savedUser.isVerified = true;
+      await savedUser.save();
+      const mockPayload = {
+        email: savedUser.email,
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(403);
+
+      expect(response.body.message).toEqual(`User already verified`);
+    });
+
+    it('should_throw_an_exception_if_code_is_incorrect', async () => {
+      await savedUser.save();
+      const mockPayload = {
+        email: savedUser.email,
+        code: 102398,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(403);
+
+      expect(response.body.message).toEqual(`Invalid code`);
+    });
+
+    it('should_throw_an_exception_if_email_is_not_defined', async () => {
+      const mockPayload = {
+        email: undefined,
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be defined');
+    });
+
+    it('should_throw_an_exception_if_email_is_empty', async () => {
+      const mockPayload = {
+        email: '',
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must not be empty');
+    });
+
+    it('should_throw_an_exception_if_email_format_is_invalid', async () => {
+      const mockPayload = {
+        email: 'invalidEmail',
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Invalid Email!');
+    });
+
+    it('should_throw_an_exception_if_email_is_not_a_string', async () => {
+      const mockPayload = {
+        email: 1234,
+        code: unverifiedUserWithOtpMock.otp,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be a string');
+    });
+
+    it('should_throw_an_exception_if_code_is_not_defined', async () => {
+      const mockPayload = {
+        email: unverifiedUserWithOtpMock.email,
+        code: undefined,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('code must be defined');
+    });
+
+    it('should_throw_an_exception_if_code_is_not_a_number', async () => {
+      const mockPayload = {
+        email: unverifiedUserWithOtpMock.email,
+        code: '1233',
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('code must be a number');
+    });
+
+    it('should_throw_an_exception_if_code_is_less_than_6_digits', async () => {
+      const mockPayload = {
+        email: unverifiedUserWithOtpMock.email,
+        code: 12345,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain(
+        'Code must have exactly 6 digits',
+      );
+    });
+
+    it('should_throw_an_exception_if_code_is_greater_than_6_digits', async () => {
+      const mockPayload = {
+        email: unverifiedUserWithOtpMock.email,
+        code: 1234567,
+      };
+      const response = await request(server)
+        .post('/auth/email/verify')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain(
+        'Code must have exactly 6 digits',
+      );
+    });
+  });
+
+  describe('login', () => {
+    let savedUser;
+
+    beforeEach(async () => {
+      const hashedPass = await bcrypt.hash(createUserDTOMock.password, 5);
+      const mockUser = {
+        ...createUserDTOMock,
+        password: hashedPass,
+        otp: unverifiedUserWithOtpMock.otp,
+        isVerified: true,
+      };
+      savedUser = await userModel.create(mockUser);
+    });
+
+    it('should_login_a_user_successfully_and_return_tokens', async () => {
+      const response = await request(server)
+        .post('/auth/login')
+        .send(loginDtoMock)
+        .expect(201);
+
+      expect(response.body.message).toEqual('Logged In successfully');
+      expect(response.body.tokens.accessToken).toEqual(expect.any(String));
+      expect(response.body.tokens.refreshToken).toEqual(expect.any(String));
+    });
+
+    it('should_throw_an_exception_if_email_is_not_correct', async () => {
+      const payload = { ...createUserDTOMock, email: 'wrongEmail@gmail.com' };
+
+      const response = await request(server)
+        .post('/auth/login')
+        .send(payload)
+        .expect(400);
+
+      expect(response.body.message).toEqual(
+        'The provided username or password is incorrect',
+      );
+    });
+
+    it('should_throw_an_exception_if_password_is_not_correct', async () => {
+      const payload = { ...createUserDTOMock, password: 'wrongPass' };
+
+      const response = await request(server)
+        .post('/auth/login')
+        .send(payload)
+        .expect(400);
+
+      expect(response.body.message).toEqual(
+        'The provided username or password is incorrect',
+      );
+    });
+
+    it('should_throw_an_exception_if_user_is_not_verified', async () => {
+      savedUser.isVerified = false;
+      await savedUser.save();
+      const response = await request(server)
+        .post('/auth/login')
+        .send(loginDtoMock)
+        .expect(403);
+
+      expect(response.body.message).toEqual('User is not verified');
+    });
+
+    it('should_throw_an_exception_if_email_is_not_defined', async () => {
+      const mockPayload = {
+        email: undefined,
+        password: loginDtoMock.password,
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockPayload)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be defined');
+    });
+
+    it('should_throw_an_exception_if_email_is_empty', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        email: '',
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must not be empty');
+    });
+
+    it('should_throw_an_exception_if_email_format_is_invalid', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        email: 'invalidEmail',
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Invalid Email!');
+    });
+
+    it('should_throw_an_exception_if_email_is_not_a_string', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        email: 1234,
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Email must be a string');
+    });
+
+    it('should_throw_an_exception_if_password_is_not_defined', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        password: undefined,
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Password must be defined');
+    });
+
+    it('should_throw_an_exception_if_password_is_empty', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        password: '',
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Password must not be empty');
+    });
+
+    it('should_throw_an_exception_if_password_is_not_a_string', async () => {
+      const mockUser = {
+        ...loginDtoMock,
+        password: 1234,
+      };
+      const response = await request(server)
+        .post('/auth/login')
+        .send(mockUser)
+        .expect(400);
+
+      expect(response.body.message).toContain('Password must be a string');
     });
   });
 });
